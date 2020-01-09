9a335cace3cd6614d867116a9f270397e421dcd5
jacobmilhon <jacob.milhon@bryanuniversity.edu>
2020-01-08 22:20:29 -0600

    week4 project

--- week4/javascript_calculator/calculator.js
+++ week4/javascript_calculator/calculator.js
@@ -5,13 +5,13 @@ let secondNumber = readline.questionInt("Please enter your second number: ");
 let selectedOperator = readline.question("Please enter the operation to perform: add, sub, mul, div: ");
 
 if(selectedOperator == "add") {
-    console.log(add(firstNumber, secondNumber));
+    console.log("The result is: " + add(firstNumber, secondNumber));
 } else if(selectedOperator == "sub") {
-    console.log(sub(firstNumber, secondNumber));
+    console.log("The result is: " + sub(firstNumber, secondNumber));
 } else if(selectedOperator == "mul") {
-    console.log(mul(firstNumber, secondNumber));
+    console.log("The result is: " + mul(firstNumber, secondNumber));
 } else if(selectedOperator == "div") {
-    console.log(div(firstNumber, secondNumber));
+    console.log("The result is: " + div(firstNumber, secondNumber));
 };
 
 function add(num1, num2) {
